bc16423755f5733ab22c9509ab3af7a07e40a6b5
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-11 23:05:05 +0500

    feat: create profiles table

--- src/schema.ts
+++ src/schema.ts
@@ -1,5 +1,10 @@
 import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
+export const profile = pgTable("profiles_table", {
+  userId: text("user_id").primaryKey(),
+  totalAura: integer("total_aura").default(0),
+});
+
 export const events = pgTable("events_table", {
   id: serial("id").primaryKey(),
   content: text("content").notNull(),
@@ -9,6 +14,9 @@ export const events = pgTable("events_table", {
   explanation: text("explanation").notNull(),
   createdAt: timestamp("created_at").notNull().defaultNow(),
 });
+
+export type InsertProfile = typeof profile.$inferInsert;
+export type SelectProfile = typeof profile.$inferSelect;
 
 export type InsertEvent = typeof events.$inferInsert;
 export type SelectEvent = typeof events.$inferSelect;
