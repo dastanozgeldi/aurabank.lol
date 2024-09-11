b30c023efa75ebdc3e0d8b779c51cf02c0f311c8
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-12 01:16:00 +0500

    fix: event margins

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -27,13 +27,16 @@ export default async function WalletPage() {
       </Card>
 
       <div className="my-4">
-        <h1 className="text-2xl font-extrabold">events</h1>
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl font-extrabold">events</h1>
+          <div>{events.length} in total</div>
+        </div>
         <div className="mt-3 h-[300px] space-y-3 overflow-auto">
           {events.length > 0 ? (
             events.map((event) => (
               <div key={event.id} className="rounded-lg border p-4">
-                <div className="flex flex-row items-center justify-between font-bold">
-                  <h2 className="mb-2 text-xl">{event.title}</h2>
+                <div className="mb-2 flex items-center justify-between font-bold">
+                  <h2 className="text-xl">{event.title}</h2>
                   <span>
                     {event.aura > 0 && "+"}
                     {event.aura}
