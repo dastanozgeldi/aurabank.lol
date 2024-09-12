5e152da269aa5eeb89d75e7a46f79d481d05bbbd
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-12 23:46:08 +0500

    fix: redirect to new url after username change

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { setUsernameAction } from "./_actions";
-import { Input } from "@/components/ui/input";
-import { FormEvent, useCallback, useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
-import { toast } from "sonner";
-import { SelectProfile } from "@/schema";
+import { User as UserIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuraCard } from "@/components/aura-card";
-import { User as UserIcon } from "lucide-react";
-import { User } from "@clerk/nextjs/server";
+import { useProfile } from "./use-profile";
 
 export default function ProfilePage({
   params,
@@ -20,47 +16,9 @@ export default function ProfilePage({
 }) {
   const username = params.username.slice(3);
   const { user: me } = useUser();
-  const [loading, setLoading] = useState(false);
-  const [profile, setProfile] = useState<SelectProfile | null>(null);
-  const [user, setUser] = useState<User | null>(null);
-  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-
-    try {
-      setIsSubmitLoading(true);
-      await setUsernameAction(formData);
-
-      toast("Username set successfully.");
-    } catch {
-      toast("Username is already taken.");
-    } finally {
-      setIsSubmitLoading(false);
-    }
-  }
-
-  const loadProfile = useCallback(async () => {
-    try {
-      setLoading(true);
-
-      const response = await fetch(`/api/${username}`);
-      const { profile, user } = await response.json();
-
-      setProfile(profile);
-      setUser(user);
-    } catch {
-      toast.error("Failed to load profile.");
-    } finally {
-      setLoading(false);
-    }
-  }, [username]);
-
-  useEffect(() => {
-    loadProfile();
-  }, [loadProfile]);
+  const { loading, profile, user, isSubmitLoading, onSubmit } =
+    useProfile(username);
 
   return (
     <div className="my-4 flex flex-col items-center justify-center">
@@ -93,10 +51,10 @@ export default function ProfilePage({
       ) : (
         profile && (
           <div className="w-full">
-            <div className="text-center text-lg">@{profile?.username}</div>
+            <div className="text-center text-lg">@{profile.username}</div>
             <div className="mt-3">
               <AuraCard
-                aura={profile!.totalAura!}
+                aura={profile.totalAura!}
                 title="Profile Total"
                 icon={<UserIcon className="h-4 w-4 text-muted-foreground" />}
               />
@@ -105,7 +63,7 @@ export default function ProfilePage({
         )
       )}
 
-      {me && profile && me?.id === profile?.userId && (
+      {me && profile && me.id === profile.userId && (
         <form onSubmit={onSubmit} className="mt-6 w-full">
           <Label htmlFor="username">new username</Label>
           <div className="flex items-center">
---
+++ src/app/[username]/use-profile.ts
@@ -0,0 +1,61 @@
+import { User } from "@clerk/nextjs/server";
+import { FormEvent, useCallback, useEffect, useState } from "react";
+import { toast } from "sonner";
+import { SelectProfile } from "@/schema";
+import { setUsernameAction } from "./_actions";
+import { useRouter } from "next/navigation";
+
+export const useProfile = (username: string) => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [profile, setProfile] = useState<SelectProfile | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [isSubmitLoading, setIsSubmitLoading] = useState(false);
+
+  const loadProfile = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch(`/api/${username}`);
+      const { profile, user } = await response.json();
+
+      setProfile(profile);
+      setUser(user);
+    } catch {
+      toast.error("Failed to load profile.");
+    } finally {
+      setLoading(false);
+    }
+  }, [username]);
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    try {
+      setIsSubmitLoading(true);
+      await setUsernameAction(formData);
+
+      toast("Username set successfully.");
+
+      router.push(`/@${formData.get("username")}`);
+    } catch {
+      toast("Username is already taken.");
+    } finally {
+      setIsSubmitLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadProfile();
+  }, [loadProfile]);
+
+  return {
+    loading,
+    profile,
+    user,
+    isSubmitLoading,
+    onSubmit,
+  };
+};
