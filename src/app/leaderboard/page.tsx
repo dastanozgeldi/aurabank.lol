f81969f146efa0a88271ffc115f244e1c83d7976
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-09-12 15:52:53 +0500

    feat: show rank of the current user on the leaderboard

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -4,16 +4,15 @@ import { profiles as profilesTable } from "@/schema";
 import { desc } from "drizzle-orm";
 
 export default async function LeaderboardPage() {
-  const profiles = await db
+  const data = await db
     .select()
     .from(profilesTable)
-    .orderBy(desc(profilesTable.totalAura))
-    .limit(5);
+    .orderBy(desc(profilesTable.totalAura));
 
   return (
     <div>
       <h1 className="my-4 text-center text-2xl font-bold">Top 5 Aura</h1>
-      <LeaderboardTable profiles={profiles} />
+      <LeaderboardTable data={data} />
     </div>
   );
 }
--- src/app/leaderboard/table.tsx
+++ src/app/leaderboard/table.tsx
@@ -1,40 +1,57 @@
+"use client";
 import {
   Table,
   TableBody,
   TableCell,
+  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { SelectProfile } from "@/schema";
+import { useUser } from "@clerk/nextjs";
 
-export function LeaderboardTable({ profiles }: { profiles: SelectProfile[] }) {
+export function LeaderboardTable({ data }: { data: SelectProfile[] }) {
+  const { user } = useUser();
+
+  const profiles = data.map((profile, index) => ({
+    ...profile,
+    rank: index + 1,
+  }));
+
+  const me = user
+    ? profiles.find((profile) => profile.userId === user.id)
+    : null;
+
   return (
     <Table>
       <TableHeader>
         <TableRow>
+          <TableHead className="w-[50px]">Rank</TableHead>
           <TableHead>User</TableHead>
           <TableHead>Aura</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody className="h-[300px] overflow-auto">
-        {profiles.map((profile) => (
+        {profiles.slice(0, 5).map((profile, index) => (
           <TableRow key={profile.userId}>
+            <TableCell>{index + 1}</TableCell>
             <TableCell className="flex items-center">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              {/* <img
-                src={user.imageUrl}
-                alt={`Profile picture of ${user.firstName} ${user.lastName}`}
-                className="h-8 w-8 rounded-full"
-              /> */}
-              <span className="ml-2">
-                {profile.username ? `@${profile.username}` : profile.userId}
-              </span>
+              {profile.username ? `@${profile.username}` : profile.userId}
             </TableCell>
             <TableCell>{profile.totalAura?.toLocaleString()}</TableCell>
           </TableRow>
         ))}
       </TableBody>
+      {me && (
+        <TableFooter>
+          <TableRow>
+            <TableCell>{me.rank}</TableCell>
+            <TableCell>{me.username}</TableCell>
+            <TableCell>{me.totalAura}</TableCell>
+          </TableRow>
+        </TableFooter>
+      )}
     </Table>
   );
 }
