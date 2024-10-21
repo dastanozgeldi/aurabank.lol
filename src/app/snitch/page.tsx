ef9ce1d9197a88fb96c1a4091929498a3b7f23f2
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-21 19:48:55 +0500

    feat: design a form and prepare hook for snitch page

--- src/app/snitch/page.tsx
+++ src/app/snitch/page.tsx
@@ -1,9 +1,39 @@
+"use client";
 import { Input } from "@/components/ui/input";
+import { useSnitch } from "./use-snitch";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { ArrowRight, Loader2 } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Page() {
+  const { loading, onSubmit } = useSnitch();
+
   return (
-    <div>
-      <Input placeholder="@cristiano" />
+    <div className="my-4">
+      <form onSubmit={onSubmit} className="space-y-3">
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="content">victim nickname</Label>
+          <Input
+            placeholder="type a user handle, e.g. @cristiano"
+            id="content"
+            name="content"
+          />
+        </div>
+        <div className="grid w-full gap-1.5">
+          <Label htmlFor="content">what happened</Label>
+          <Textarea
+            placeholder="he farted during the class"
+            id="content"
+            name="content"
+          />
+        </div>
+        <Button disabled={loading} className="w-full">
+          {loading && <Loader2 className="mr-2 h-5 w-5 animate-spin" />}
+          Submit
+          {!loading && <ArrowRight className="ml-2 h-4 w-4" />}
+        </Button>
+      </form>
     </div>
   );
 }
---
+++ src/app/snitch/use-snitch.ts
@@ -0,0 +1,32 @@
+import { useRouter } from "next/navigation";
+import { useState, FormEvent } from "react";
+import { toast } from "sonner";
+
+export const useSnitch = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    try {
+      setLoading(true);
+      console.log(formData);
+      // await addSnitchAction(formData);
+
+      toast.success("Event was added.");
+    } catch {
+      toast.error("Failed to add the event.");
+    } finally {
+      setLoading(false);
+      router.refresh();
+    }
+  }
+
+  return {
+    loading,
+    onSubmit,
+  };
+};
