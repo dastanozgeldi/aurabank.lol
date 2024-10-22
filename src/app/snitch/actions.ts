ab8366c12707602ffff0076f60d1e95691e53b6d
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-22 17:49:51 +0500

    feat: snitch action setup

--- src/app/snitch/actions.ts
+++ src/app/snitch/actions.ts
@@ -1,5 +1,36 @@
 "use server";
 
+import { generateEventAssessment } from "@/lib/utils";
+import { profilesTable, snitchesTable } from "@/schema";
+import { db } from "@/server/db";
+import { insertEvent } from "@/server/queries";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+
 export async function addSnitchAction(formData: FormData) {
-  console.log(formData);
+  const { userId: culpritId } = auth();
+  if (!culpritId) throw new Error("Unauthorized");
+
+  const username = formData.get("username") as string;
+  const victim = await db
+    .select()
+    .from(profilesTable)
+    .where(eq(profilesTable.username, username));
+
+  if (!victim) throw new Error("Victim not found");
+
+  const content = formData.get("content") as string;
+  const assessment = await generateEventAssessment(content);
+
+  const { event } = await insertEvent({
+    userId: victim.userId,
+    content,
+    assessment,
+  });
+
+  await db.insert(snitchesTable).values({
+    culpritId,
+    victimId: victim.userId,
+    eventId: event.id,
+  });
 }
