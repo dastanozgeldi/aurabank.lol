703c2b834ff38207531a4a9167d531214a763294
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-10-23 15:56:11 +0500

    feat: username selector component

---
+++ src/app/snitch/username-selector.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { CheckIcon, ChevronDownIcon, AtSignIcon } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { SelectProfile } from "@/server/schema";
+import { useState } from "react";
+
+export default function UsernameSelector({
+  profiles,
+}: {
+  profiles: SelectProfile[];
+}) {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between"
+        >
+          {value ? (
+            <span className="flex items-center">
+              <AtSignIcon className="mr-2 h-4 w-4 text-muted-foreground" />
+              {value}
+            </span>
+          ) : (
+            "select username..."
+          )}
+          <ChevronDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="PopoverContent p-0">
+        <Command>
+          <CommandInput
+            placeholder="search a username, e.g. cristiano"
+            className="h-9"
+          />
+          <CommandList>
+            <CommandEmpty>No username found.</CommandEmpty>
+            <CommandGroup>
+              {profiles.map(
+                ({ username }) =>
+                  username && (
+                    <CommandItem
+                      key={username}
+                      value={username}
+                      onSelect={(currentValue: string) => {
+                        setValue(currentValue === value ? "" : currentValue);
+                        setOpen(false);
+                      }}
+                      className="flex w-full items-center gap-2 px-4 py-2"
+                    >
+                      @{username}
+                      <CheckIcon
+                        className={cn(
+                          "ml-auto h-4 w-4",
+                          value === username ? "opacity-100" : "opacity-0",
+                        )}
+                      />
+                    </CommandItem>
+                  ),
+              )}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
