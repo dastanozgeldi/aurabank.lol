5051c2b155e4c8e1f1fb1ea79d1f5bd1cebc3591
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-07-30 19:51:00 +0500

    feat: insert subscription db query

--- src/features/subscription/db.ts
+++ src/features/subscription/db.ts
@@ -19,3 +19,28 @@ export async function hasActiveSubscription(userId: string): Promise<boolean> {
   const subscription = await getActiveSubscription(userId);
   return subscription.length > 0;
 }
+
+export async function insertSubscription(
+  userId: string,
+  {
+    polarSubscriptionId,
+    startDate,
+    price,
+  }: {
+    polarSubscriptionId: string;
+    startDate: Date;
+    price: number;
+  },
+) {
+  const [newSubscription] = await db
+    .insert(subscriptionsTable)
+    .values({
+      userId,
+      polarSubscriptionId,
+      startDate,
+      price,
+    })
+    .returning();
+
+  return newSubscription;
+}
