29397ad1be8691c4d8424bdafdb8926166a261d0
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-07-30 10:32:37 +0500

    feat: handle subscription revoke event

--- src/app/api/webhooks/polar/route.ts
+++ src/app/api/webhooks/polar/route.ts
@@ -4,7 +4,11 @@ import { Webhooks } from "@polar-sh/nextjs";
 export const POST = Webhooks({
   webhookSecret: env.POLAR_WEBHOOK_SECRET,
   onSubscriptionActive: async (payload) => {
-    console.log("just had a user subscription become active");
+    console.log("user subscription active");
+    console.log(payload);
+  },
+  onSubscriptionRevoked: async (payload) => {
+    console.log("user subscription revoked");
     console.log(payload);
   },
 });
