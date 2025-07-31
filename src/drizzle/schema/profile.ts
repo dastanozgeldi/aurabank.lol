c591a8387bfcc7a01fb3914647e4e41fcc9d4de2
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-07-31 17:44:45 +0500

    feat: event profile relations

--- src/drizzle/schema/profile.ts
+++ src/drizzle/schema/profile.ts
@@ -5,6 +5,8 @@ import {
   timestamp,
   varchar,
 } from "drizzle-orm/pg-core";
+import { eventsTable } from "./event";
+import { relations } from "drizzle-orm";
 
 export const profilesTable = pgTable("profiles_table", {
   userId: text("user_id").primaryKey(),
@@ -12,6 +14,10 @@ export const profilesTable = pgTable("profiles_table", {
   username: varchar("username", { length: 20 }).unique(),
   createdAt: timestamp("created_at").defaultNow(),
 });
+
+export const profilesRelations = relations(profilesTable, ({ many }) => ({
+  events: many(eventsTable),
+}));
 
 export type InsertProfile = typeof profilesTable.$inferInsert;
 export type SelectProfile = typeof profilesTable.$inferSelect;
