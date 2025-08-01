99987fbf530ba442d9b07eb0004b9b2776f0bc99
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-01 13:55:01 +0500

    feat: add new fields to profile schema

---
+++ src/drizzle/migrations/0003_futuristic_quasar.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "profiles_table" ADD COLUMN "image_url" text;--> statement-breakpoint
+ALTER TABLE "profiles_table" ADD COLUMN "deleted_at" timestamp;
--- src/drizzle/schema/profile.ts
+++ src/drizzle/schema/profile.ts
@@ -5,7 +5,9 @@ import { relations } from "drizzle-orm";
 export const profilesTable = pgTable("profiles_table", {
   userId: text("user_id").primaryKey(),
   username: varchar("username", { length: 20 }).unique().notNull(),
+  imageUrl: text("image_url"),
   createdAt: timestamp("created_at").defaultNow(),
+  deletedAt: timestamp("deleted_at"),
 });
 
 export const profilesRelations = relations(profilesTable, ({ many }) => ({
