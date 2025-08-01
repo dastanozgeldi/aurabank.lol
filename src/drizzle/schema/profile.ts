acb40bde8089ce7e396592f83c4ee5ad6d1610a7
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-01 14:06:36 +0500

    feat: add name field to profile schema

--- src/drizzle/schema/profile.ts
+++ src/drizzle/schema/profile.ts
@@ -4,6 +4,7 @@ import { relations } from "drizzle-orm";
 
 export const profilesTable = pgTable("profiles_table", {
   userId: text("user_id").primaryKey(),
+  name: text("name"),
   username: varchar("username", { length: 20 }).unique().notNull(),
   imageUrl: text("image_url"),
   createdAt: timestamp("created_at").defaultNow(),
