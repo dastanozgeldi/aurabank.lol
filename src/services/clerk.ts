219bc4f68ff0624bcaad59a80be591a0adec1200
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-01 18:40:59 +0500

    feat: get rid of onboarding

--- src/services/clerk.ts
+++ src/services/clerk.ts
@@ -2,23 +2,6 @@ import { clerkClient } from "@clerk/nextjs/server";
 
 const client = await clerkClient();
 
-export function syncClerkUserMetadata({
-  userId,
-  username,
-  onboardingComplete,
-}: {
-  userId: string;
-  username?: string;
-  onboardingComplete?: boolean;
-}) {
-  client.users.updateUserMetadata(userId, {
-    publicMetadata: {
-      username,
-      onboardingComplete,
-    },
-  });
-}
-
 export function getUser(userId: string) {
   return client.users.getUser(userId);
 }
