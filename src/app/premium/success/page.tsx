b5515a341c20ac394440bd339c809681b51fa375
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-01 18:15:47 +0500

    chore: unused imports

--- src/app/premium/success/page.tsx
+++ src/app/premium/success/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import { Button } from '@/components/ui/button';
 
 export default function SuccessPage() {
   return (
-    <div className="-mb-4 h-screen flex flex-col items-center justify-center">
+    <div className="-mb-4 flex h-screen flex-col items-center justify-center">
       <Image src="/og-image.png" alt="success image" width={400} height={400} />
-      <h3 className="text-2xl mt-8 text-center">thank you for purchasing <br /> aurabank subscription!</h3>
+      <h3 className="mt-8 text-center text-2xl">
+        thank you for purchasing <br /> aurabank subscription!
+      </h3>
     </div>
   );
 }
