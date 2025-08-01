74bc056aa89bb4c8db7d73ae74b1aadced83dd3e
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-01 19:15:17 +0500

    feat: rename clerk webhook var

--- src/data/env/server.ts
+++ src/data/env/server.ts
@@ -9,7 +9,7 @@ export const env = createEnv({
     POLAR_PRODUCT_ID: z.string().min(1),
     POLAR_WEBHOOK_SECRET: z.string().min(1),
     POLAR_SERVER: z.enum(["sandbox", "production"]).default("production"),
-    CLERK_WEBHOOK_SECRET: z.string().min(1),
+    CLERK_WEBHOOK_SIGNING_SECRET: z.string().min(1),
   },
   experimental__runtimeEnv: process.env,
 });
